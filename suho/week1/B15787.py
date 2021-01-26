d7aa65e7a24c5197de9a124ac16e609edce66594
leesh96 <suho2718@gmail.com>
2021-01-26 21:28:11 +0900

    [PS] week1 구현

---
+++ suho/week1/B15787.py
@@ -0,0 +1,39 @@
+import sys
+from collections import deque
+
+N, M = map(int, sys.stdin.readline().rstrip().split())
+control = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(M)]
+train = [[0] * 20 for _ in range(N)]
+pass_train = []
+
+for order in control:
+    case = order[0]
+    i = order[1] - 1
+    if case == 1:
+        x = order[2] - 1
+        if train[i][x] == 0:
+            train[i][x] = 1
+    if case == 2:
+        x = order[2] - 1
+        if train[i][x] == 1:
+            train[i][x] = 0
+    if case == 3:
+        Q = deque(train[i])
+        Q.pop()
+        Q.appendleft(0)
+        train[i] = list(Q)
+    if case == 4:
+        Q = deque(train[i])
+        Q.popleft()
+        Q.append(0)
+        train[i] = list(Q)
+
+count = 1
+pass_train.append(train[0])
+for i in range(1, N):
+    if train[i] in pass_train:
+        continue
+    else:
+        pass_train.append(train[i])
+        count += 1
+print(count)
---
+++ suho/week1/B16235.py
@@ -0,0 +1,55 @@
+import sys
+
+N, M, K = map(int, sys.stdin.readline().rstrip().split())
+A = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(N)]
+ground = [[5] * N for _ in range(N)]
+tree_age = [[[] for _ in range(N)] for _ in range(N)]
+for _ in range(M):
+    r, c, age = map(int, sys.stdin.readline().rstrip().split())
+    tree_age[r-1][c-1].append(age)
+
+dir = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+
+for _ in range(K):
+    # 봄, 여름
+    for i in range(N):
+        for j in range(N):
+            if tree_age[i][j]:
+                tree_age[i][j].sort()
+                new_tree_age = []
+                dead_trees = 0
+                for age in tree_age[i][j]:
+                    if age > ground[i][j]:
+                        dead_trees += age // 2
+                    else:
+                        ground[i][j] -= age
+                        age += 1
+                        new_tree_age.append(age)
+                tree_age[i][j] = new_tree_age
+                ground[i][j] += dead_trees
+
+    if not tree_age:
+        print(0)
+        sys.exit()
+
+    # 가을
+    for i in range(N):
+        for j in range(N):
+            if tree_age[i][j]:
+                for age in tree_age[i][j]:
+                    if age % 5 == 0:
+                        for x, y in dir:
+                            next_r = i + x
+                            next_c = j + y
+                            if (0 <= next_r < N) and (0 <= next_c < N):
+                                tree_age[next_r][next_c].append(1)
+            # 겨울
+            ground[i][j] += A[i][j]
+
+t_cnt = 0
+for i in range(N):
+    for j in range(N):
+        if tree_age[i][j]:
+            t_cnt += len(tree_age[i][j])
+
+print(t_cnt)
