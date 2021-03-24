fd8b14e8db1e4105aa4cbae06a0aeff74b138383
jaeseuk <sjs4126@naver.com>
2021-03-24 21:36:06 +0900

    [solve] week7 - shortest path

---
+++ jaeseuk/최단경로/q37.py
@@ -0,0 +1,27 @@
+from sys import stdin
+
+INF = int(1e9)
+n = int(stdin.readline().rstrip())
+m = int(stdin.readline().rstrip())
+
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    graph[i][i] = 0
+
+for _ in range(m):
+    a, b, c = map(int, stdin.readline().rstrip().split())
+    graph[a][b] = min(graph[a][b], c)
+
+for c in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][c] + graph[c][b])
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if graph[i][j] == INF:
+            print(0, end=' ')
+        else:
+            print(graph[i][j], end=' ')
+    print()
