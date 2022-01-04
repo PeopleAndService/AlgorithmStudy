03a7e36a654d59142ab169cd948fa319cbb82d2a
Yoongyeong Song <14dnfnfn@gmail.com>
2022-01-04 23:12:11 +0900

    [Study] 프로그래머스 L1 내적 외 2문제

--- yoongyeong/by_kotlin/src/main/kotlin/programmers/Level1.kt
+++ yoongyeong/by_kotlin/src/main/kotlin/programmers/Level1.kt
@@ -1,8 +1,9 @@
 package programmers
 
 import kotlin.math.abs
+import kotlin.math.pow
 
-fun main(args:Array<String>):Unit{
+fun main(args: Array<String>): Unit {
     println(solution01(-1, -3))
     println(solution01x01(1, 4))
 }
@@ -10,7 +11,7 @@ fun main(args:Array<String>):Unit{
 // 두 정수 사이의 합
 fun solution01(a: Int, b: Int): Long {
     var answer: Long = 0
-    if(a < b) {
+    if (a < b) {
         for (i in a..b) answer += i
     } else {
         for (i in b..a) answer += i
@@ -26,10 +27,10 @@ fun solution01x02(a: Int, b: Int): Long =
     (abs(a - b) + 1L) * (a + b) / 2L
 
 // 없는 숫자 더하기
-fun solution02(numbers: IntArray) =  45 - numbers.sum()
+fun solution02(numbers: IntArray) = 45 - numbers.sum()
 
 // 평균 구하기
-fun solution03(arr: IntArray) =  arr.sum() / arr.size.toDouble()
+fun solution03(arr: IntArray) = arr.sum() / arr.size.toDouble()
 
 fun solution03x01(arr: IntArray) = arr.average()
 
@@ -37,7 +38,7 @@ fun solution03x01(arr: IntArray) = arr.average()
 fun solution04(absolutes: IntArray, signs: BooleanArray): Int {
     var answer = 0
     for (i in signs.indices) {
-        if (signs[i]) answer+= absolutes[i]
+        if (signs[i]) answer += absolutes[i]
         else answer -= absolutes[i]
     }
     return answer
@@ -50,4 +51,41 @@ fun solution04x02(absolutes: IntArray, signs: BooleanArray): Int {
     val toIntSignsArray = signs.map { if (it) 1 else -1 }
     return absolutes.zip(toIntSignsArray).map { it.first * it.second }.sum()
 }
+
+// 내적
+fun solution05(a: IntArray, b: IntArray) =
+    a.zip(b).map { it.first * it.second }.sum()
+
+// 나머지가 1이 되는 수 찾기
+fun solution06(n: Int): Int {
+    for (x in 2..n) {
+        if (n % x == 1) return x
+    }
+    return n
+}
+
+fun solution07(n: Int): Int {
+    var answer = 0
+    var number = n
+    val size = n.toString().length
+    for (x in 1..size) {
+        val share: Int = number / (10.0).pow(size-x).toInt()
+        answer += share
+        number -= share * (10.0).pow(size-x).toInt()
+    }
+    return answer
+}
+
+fun solution07x01(n: Int): Int {
+    var answer = 0
+    for (nn in n.toString()) {
+        answer += (nn.toString()).toInt()
+    }
+    return answer
+}
+
+// 짝수와 홀수
+fun solution08(num: Int) = if (num % 2 == 0) "Even" else "Odd"
+
+fun solution08x01(num: Int) = if (num.and(1) == 0) "Even" else "Odd"
 
