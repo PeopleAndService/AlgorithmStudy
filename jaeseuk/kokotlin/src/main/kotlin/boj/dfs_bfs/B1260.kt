b5330d95caf70af18a885ddf206b72a95e096ad3
jaeseuk <sjs4126@naver.com>
2022-01-14 23:11:05 +0900

    [PS] 백준 dfs,bfs 1260

---
+++ jaeseuk/kokotlin/src/main/kotlin/boj/dfs_bfs/B1260.kt
@@ -0,0 +1,48 @@
+package boj.dfs_bfs
+
+import java.util.*
+
+fun main() {
+    val (n, m, v) = readLine()!!.split(' ').map { it.toInt() }
+    val graph = List<MutableList<Int>>(n + 1) { mutableListOf() }
+    val visited = MutableList(n + 1){ false }
+
+    repeat(m) {
+        val (a, b) = readLine()!!.split(' ').map { it.toInt() }
+        graph[a].add(b)
+        graph[b].add(a)
+    }
+    graph.forEach { it.sort() }
+
+    dfs(graph, visited, v)
+    visited.fill(false)
+    println()
+    bfs(graph, visited, v)
+}
+
+fun dfs(graph: List<MutableList<Int>>, visited: MutableList<Boolean>, start: Int) {
+    print("$start ")
+    visited[start] = true
+
+    for (v in graph[start]) {
+        if (!visited[v]) dfs(graph, visited, v)
+    }
+}
+
+fun bfs(graph: List<MutableList<Int>>, visited: MutableList<Boolean>, start: Int) {
+    val q = LinkedList<Int>()
+    q.add(start)
+
+    while (q.isNotEmpty()) {
+        val now = q.poll()
+
+        if (!visited[now]) {
+            print("$now ")
+            visited[now] = true
+        }
+
+        for (v in graph[now]) {
+            if (!visited[v]) q.add(v)
+        }
+    }
+}
