4f1aa63bcac636418a8b2f3311fea976ca8fc6e1
Lee Suho <suho2718@gmail.com>
2022-01-21 22:26:19 +0900

    [Solve] 22.01.21. 1주차 스택,큐,덱 문제 풀이
    1158 요세푸스 문제

---
+++ suho/withkotlin/src/main/kotlin/week1/B1158.kt
@@ -0,0 +1,33 @@
+package week1
+
+import java.util.*
+
+/*
+채점 결과 : 628ms/299512KB
+풀이 날짜 : 2022/01/21
+*/
+
+fun main() {
+    val (n, k) = readln().split(" ").map { it.toInt() }
+    val queue = LinkedList<Int>()
+    repeat(n) {
+        queue.offer(it + 1)
+    }
+
+    System.out.bufferedWriter().use { bw ->
+        bw.write("<")
+        while (queue.size != 1) {
+            bw.write("${queue.printOrder(k)}, ")
+        }
+        bw.write("${queue.poll()}>")
+        bw.flush()
+    }
+}
+
+fun <T : Number> Queue<T>.printOrder(k: Int): T {
+    repeat(k - 1) {
+        this.offer(this.poll())
+    }
+
+    return this.poll()
+}
