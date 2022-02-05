328f5f2478b5b67999687e4c8f8db55860ed2c7c
Yoongyeong Song <14dnfnfn@gmail.com>
2022-02-05 12:37:23 +0900

    [Solve] backtracking 4문제 (n과 m 1번부터 4번까지)

---
+++ yoongyeong/by_kotlin/src/main/kotlin/backTracking_recursion/B15649.kt
@@ -0,0 +1,32 @@
+package backTracking_recursion
+
+import java.util.*
+
+// N과 M (1)
+
+private lateinit var array: IntArray
+private lateinit var visited: BooleanArray
+private val sb = StringBuilder()
+
+fun main() {
+    val (n, m) = readLine()!!.split(" ").map { it.toInt() }
+    array = IntArray(m); visited = BooleanArray(n)
+
+    permutation(n, m)
+    println(sb.toString())
+}
+
+private fun permutation(n: Int, m: Int, depth: Int = 0) {
+    if (depth == m) {
+        sb.append(array.joinToString(" ", postfix = "\n")); return
+    }
+
+    for (i in 0 until n) {
+        if (!visited[i]) {
+            visited[i] = true
+            array[depth] = i + 1
+            permutation(n, m, depth + 1)
+            visited[i] = false
+        }
+    }
+}
