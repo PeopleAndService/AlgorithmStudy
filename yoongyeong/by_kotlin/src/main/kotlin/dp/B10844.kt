4d8947acd93cf1db561e5e8047e0acac6f9309ec
Yoongyeong Song <14dnfnfn@gmail.com>
2022-02-21 12:32:43 +0900

    [Solve] dp 쉬운 계단 수

---
+++ yoongyeong/by_kotlin/src/main/kotlin/dp/B10844.kt
@@ -0,0 +1,18 @@
+package dp
+
+// 쉬운 계단 수
+
+fun main() {
+    val n = System.`in`.bufferedReader().readLine().toInt()
+    val stair = Array(n) {LongArray(10) {1L} }; stair[0][0] = 0L
+    for (i in 1 until n) {
+        for (j in 0 until 10) {
+            stair[i][j] = when(j) {
+                0 -> stair[i-1][1]
+                9 -> stair[i-1][8]
+                else -> (stair[i-1][j-1] + stair[i-1][j+1])
+            } % 1_000_000_000
+        }
+    }
+    print(stair[n-1].sum() % 1_000_000_000)
+}
