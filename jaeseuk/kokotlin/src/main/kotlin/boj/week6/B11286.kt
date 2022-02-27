701189db4d76ce49553c999f0f6b13e274f188d2
jaeseuk <sjs4126@naver.com>
2022-02-27 20:05:29 +0900

    [Solve] 백준
    2343
    11286

---
+++ jaeseuk/kokotlin/src/main/kotlin/boj/week6/B11286.kt
@@ -0,0 +1,47 @@
+package boj.week6
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+import kotlin.math.abs
+
+//  기타 레슨
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val compare = { a: Int, b: Int ->
+        when {
+            a > b -> 1
+            a < b -> -1
+            else -> 0
+        }
+    }
+    val q = PriorityQueue { a: Pair<Int, Int>, b: Pair<Int, Int> ->
+        when {
+            a.first != b.first -> compare(a.first, b.first)
+            else -> compare(a.second, b.second)
+        }
+    }
+
+    repeat(br.readLine().toInt()) {
+        val op = br.readLine().toInt()
+
+        if (op == 0) {
+            if (q.isEmpty()) {
+                bw.append("0")
+            } else {
+                bw.append("${q.poll().second}")
+            }
+            bw.newLine()
+        } else {
+            q.add(abs(op) to op)
+        }
+    }
+
+    bw.flush()
+    bw.close()
+}
