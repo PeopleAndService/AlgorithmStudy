c4bd6d99344e8d4aed813d1b633173615275c531
Yoongyeong Song <14dnfnfn@gmail.com>
2022-03-25 21:41:10 +0900

    [Solve] 플로이드 와샬 = 키순서

---
+++ yoongyeong/by_kotlin/src/main/kotlin/topologicalsort_floydwashal/B2458.kt
@@ -0,0 +1,51 @@
+package topologicalsort_floydwashal
+
+import java.util.StringTokenizer
+
+// 키 순서
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val n: Int; val m: Int
+    with(StringTokenizer(br.readLine())) {
+        n = nextToken().toInt(); m = nextToken().toInt()
+    }
+    val students = Array(n+1) { BooleanArray(n+1) }
+    val studentsR = Array(n+1) { BooleanArray(n+1) }
+
+    repeat(m) {
+        with(StringTokenizer(br.readLine())) {
+            val a = nextToken().toInt()
+            val b = nextToken().toInt()
+            students[a][b] = true
+            studentsR[b][a] = true
+        }
+    }
+
+    for (k in 1 .. n) {
+        for (i in 1 .. n) {
+            for (j in 1 .. n) {
+                if (students[i][k] && students[k][j]) students[i][j] = true
+                if (studentsR[i][k] && studentsR[k][j]) studentsR[i][j] = true
+            }
+        }
+    }
+
+    for (i in 1 .. n) {
+        for (j in 1 .. n) {
+            students[i][j] = students[i][j] || studentsR[i][j]
+        }
+    }
+
+    var answer = 0
+
+    scope@for (i in 1 .. n) {
+        for (j in 1 .. n) {
+            if (i == j) continue
+            if (!students[i][j]) continue@scope
+        }
+        answer ++
+    }
+
+    print(answer)
+}
