7dc250ca8db4e60866864de034aabfce09a7a1ad
Yoongyeong Song <14dnfnfn@gmail.com>
2022-04-17 22:57:31 +0900

    [Solve] 누적합 - 구간 합 구하기 4

---
+++ yoongyeong/by_kotlin/src/main/kotlin/twopointer_prefixsum/B11659.kt
@@ -0,0 +1,33 @@
+package twopointer_prefixsum
+
+import java.util.StringTokenizer
+
+// 구간 합 구하기 4
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val bw = System.out.bufferedWriter()
+
+    val n: Int; val m: Int
+    with(StringTokenizer(br.readLine())) {
+        n = nextToken().toInt()
+        m = nextToken().toInt()
+    }
+    val dp = IntArray(n+1)
+    with(StringTokenizer(br.readLine())) {
+        for (index in 1 .. n) {
+            dp[index] = dp[index-1] + nextToken().toInt()
+        }
+    }
+
+    repeat(m) {
+        with(StringTokenizer(br.readLine())) {
+            val i = nextToken().toInt()
+            val j = nextToken().toInt()
+            bw.write("${dp[j] - dp[i-1]}\n")
+        }
+    }
+
+    bw.close()
+    br.close()
+}
